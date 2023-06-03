2ebb82735e85ecb7c5dc4b0f6cfa5ecab6a3af5d
Abhishek Kumar Singh <abhisheksingh17150@gmail.com>
2023-06-04 00:41:39 +0530

    create component

--- App.js
+++ App.js
@@ -1,32 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {}, "Namaste Everyone!");
+const heading = <h1>"Namaste Everyone!"</h1>
 
-    const heading1 = React.createElement(
-        "h1", 
-        {
-            id: 'title',
-            key: 'h1',
-        },
-        "Heading 1 from parcel"
-    );
+const HeadingComponent = () => {
+    return (
+        <div>
+            {heading}
+            <h2>Hi Abhishek </h2>
+            <h3>How are you!</h3>
+        </div>
 
-    const heading2 = React.createElement(
-        "h2", 
-        {
-            id: 'title',
-            key: 'h2',
-        },
-        "Heading 2"
-    );
+    )
+}
 
-    const container = React.createElement(
-        "div", 
-        {
-            id: 'container'
-        },
-        [heading1, heading2]
-    );
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(container);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />);
